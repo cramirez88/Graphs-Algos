ce2348d14e0bd9c51599da6c75f157746b0a5b59
Christian Ramirez <christian.r0602@gmail.com>
2022-12-06 21:29:29 -0600

    has Path breadht first

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const hasPath = (graph, src, dst) => {
-  let queue = [src] //f
+// const hasPath = (graph, src, dst) => {
+//   let queue = [src] //f
   
+//   while(queue.length > 0){
+//     let current = queue.shift() //return f
+//     if(current === dst) return true
+    
+//     for (let neighbor of graph[current]){
+//       queue.push(neighbor)
+//     }
+//   }
+//   return false
+// };
+
+const hasPath = (graph, src, dst) => {
+  const queue = [src]
   while(queue.length > 0){
-    let current = queue.shift() //return f
+    const current = queue.shift()
     if(current === dst) return true
     
-    for (let neighbor of graph[current]){
+    for(let neighbor of graph[current]){
       queue.push(neighbor)
     }
   }
