702e7ee07ab9032ef76d2f4d1dc97b34cd8e5428
Christian Ramirez <christian.r0602@gmail.com>
2022-12-07 21:48:06 -0600

    undirected path recursive

--- script.js
+++ script.js
@@ -12,15 +12,49 @@
 //   return false
 // };
 
-const hasPath = (graph, src, dst) => {
-  const queue = [src]
-  while(queue.length > 0){
-    const current = queue.shift()
-    if(current === dst) return true
+// const hasPath = (graph, src, dst) => {
+//   const queue = [src]
+//   while(queue.length > 0){
+//     const current = queue.shift()
+//     if(current === dst) return true
     
-    for(let neighbor of graph[current]){
-      queue.push(neighbor)
+//     for(let neighbor of graph[current]){
+//       queue.push(neighbor)
+//     }
+//   }
+//   return false
+// };
+
+const undirectedPath = (edges, nodeA, nodeB) => {
+  const graph = createGraph(edges)
+  return hasPath(graph, nodeA, nodeB, new Set())
+};
+
+const createGraph = (edges) => {
+const graph = {}
+
+for(let edge of edges){
+  const [a, b] = edge
+  if(!(a in graph)) graph[a] = []
+  if(!(b in graph)) graph[b] = []
+
+  graph[a].push(b)
+  graph[b].push(a)
+}
+  return graph
+};
+
+
+const hasPath = (graph, src, dst, visited) => {
+  if(src === dst) return true
+  if(visited.has(src)) return false
+  visited.add(src)
+  
+  for(let neighbor of graph[src]){
+    if(hasPath(graph, neighbor, dst, visited) === true) {
+      return true
     }
   }
   return false
-};
+}
+
